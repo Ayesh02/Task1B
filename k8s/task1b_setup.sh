a3990cef4b90593b85c3efe589d33ce4fa471d7f
Shasha <ayeshabegum02@gmail.com>
2023-04-18 00:59:38 +0800

    remove unused and fixes

--- k8s/task1b_setup.sh
+++ k8s/task1b_setup.sh
@@ -1,1 +1,17 @@
 # Place your commands here
+kind create cluster --name kind-1 --config k8s/kind/cluster-config.yaml
+kubectl cluster-info
+kubectl get nodes
+
+kubectl get nodes -L ingress-ready
+kubectl apply -f k8s/manifests/k8s/backend-deployment.yaml
+kubectl get deployment/backend --watch
+kubectl get pods
+kubectl apply -f k8s/manifests/k8s/backend-service.yaml
+kubectl get svc
+
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
+kubectl -n ingress-nginx get deploy -w
+kubectl get pods -n ingress-nginx
+kubectl apply -f k8s/manifests/k8s/backend-ingress.yaml
+kubectl get ingress
